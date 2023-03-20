158d92e84c548c5516be1044be1984e3a89dc16b
UmarpreetGrover <umarpreetgrover54@gmail.com>
2023-03-20 23:09:28 +0530

    defining student data

--- sql.py
+++ sql.py
@@ -1,3 +1,31 @@
 import mysql.connector as a
 
 con=a.connect(host="localhost",user="root",password="Tritiya12345@",database="university")
+def ast():
+    
+    a=input("enter the roll no of the student::")
+
+    b=input("enter the name of the student::")
+
+    z=input("enter the semester::")
+
+    d=input("enter the section of the student::")
+
+    e=input("enter the address of the student::")
+
+    f=input("enter the phoneno of the student::")
+
+    data=(a,b,z,d,e,f)
+
+    sql='insert into student values(%s,%s,%s,%s,%s,%s)'
+
+    c=con.cursor()
+
+    c.execute(sql,data)
+
+    con.commit()
+
+    print("data entered successfully")
+
+    print(">---------------------------------------------------------------------------------------------------------<")
+
